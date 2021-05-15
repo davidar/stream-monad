e00f07944965412bd7a667235d1602c0ade55cd5
David A Roberts <d@vidr.cc>
2021-05-15 02:38:48 +0000

    more tests

--- test/microkanren.hs
+++ test/microkanren.hs
@@ -4,7 +4,7 @@ import Control.Monad (MonadPlus(..))
 import qualified Control.Monad.Stream as Stream
 import Control.Monad.Stream (Stream)
 import Control.Monad.State (MonadState(..), StateT(..), execStateT, mapStateT)
-import Test.Hspec
+import Test.Hspec (hspec, it, shouldBe)
 
 type Var = Integer
 type Subst = [(Var, Term)]
@@ -14,10 +14,12 @@ type Program a = StateT State Stream a
 data Term = Atom String | Pair Term Term | Var Var deriving (Eq, Show)
 
 -- Apply a substitution to the top level of a term
+walk :: Term -> Subst -> Term
 walk (Var v) s = case lookup v s of Nothing -> Var v
                                     Just us -> walk us s
 walk u s = u
 
+extS :: Var -> Term -> Subst -> Subst
 extS x v s = (x, v) : s
 
 -- Try to unify two terms under a substitution;
@@ -39,7 +41,7 @@ fresh = do
   put (s, c+1)
   return (Var c)
 
--- MicroKanren program formers
+-- microKanren program formers
 zzz :: Program a -> Program a
 zzz = mapStateT Stream.suspended
 
@@ -59,24 +61,58 @@ disj = (<|>)
 conj :: Program a -> Program b -> Program b
 conj = (>>)
 
+-- Recovering miniKanren interface
+reify :: [State] -> [Term]
+reify = map reifyState
+  where
+    reifyState :: State -> Term
+    reifyState (s, _) = let v = walk' (Var 0) s in walk' v (reifyS v [])
 
-five = callFresh (\x -> equiv x (Atom "5"))
+    reifyS :: Term -> Subst -> Subst
+    reifyS v s = case walk v s of
+      Var v -> let n = reifyName (length s) in (v, n) : s
+      Pair u v -> reifyS v $ reifyS u s
+      _ -> s
 
-fives_ x = disj (equiv x (Atom "5")) (zzz $ fives_ x)
-fives = callFresh fives_
+    reifyName :: Int -> Term
+    reifyName n = Atom $ "_." ++ show n
 
-fivesRev_ x = disj (zzz $ fivesRev_ x) (equiv x (Atom "5"))
-fivesRev = callFresh fivesRev_
+    walk' :: Term -> Subst -> Term
+    walk' v s = case walk v s of
+      Pair u v -> Pair (walk' u s) (walk' v s)
+      v -> v
 
-ab = conj
-  (callFresh (\a -> equiv a (Atom "7")))
-  (callFresh (\b -> disj (equiv b (Atom "5")) (equiv b (Atom "6"))))
+callEmptyState :: Program () -> Stream State
+callEmptyState g = execStateT g ([], 0)
 
-runTest p = Stream.observeAll (execStateT p ([], 0))
+run :: Int -> (Term -> Program ()) -> [Term]
+run n = reify . Stream.observeMany n . callEmptyState . callFresh
 
+run' :: (Term -> Program ()) -> [Term]
+run' = reify . Stream.observeAll . callEmptyState . callFresh
+
+-- Tests
+main :: IO ()
 main = hspec $ do
-  it "five" $ runTest five `shouldBe` [([(0,Atom "5")],1)]
-  it "fives" $ take 10 (runTest fives) `shouldBe` replicate 10 ([(0,Atom "5")],1)
-  it "fivesRev" $ take 10 (runTest fivesRev) `shouldBe` replicate 10 ([(0,Atom "5")],1)
-  it "ab" $ runTest ab `shouldBe`
-    [([(1,Atom "5"),(0,Atom "7")],2),([(1,Atom "6"),(0,Atom "7")],2)]
+  let ab = conj
+        (callFresh (\a -> equiv a (Atom "7")))
+        (callFresh (\b -> disj (equiv b (Atom "5")) (equiv b (Atom "6"))))
+      five x = equiv x (Atom "5")
+      fives x = disj (equiv x (Atom "5")) (zzz $ fives x)
+      fivesRev x = disj (zzz $ fivesRev x) (equiv x (Atom "5"))
+      sixes x = disj (equiv x (Atom "6")) (zzz $ sixes x)
+      p56 x = disj (fives x) (sixes x)
+      p010 q = do
+        x <- fresh; y <- fresh
+        equiv q (Pair x (Pair y x)) <|> equiv q (Pair y (Pair x y))
+  it "ab" $ Stream.observeAll (callEmptyState ab) `shouldBe`
+    [([(1,Atom "5"),(0,Atom "7")],2)
+    ,([(1,Atom "6"),(0,Atom "7")],2)]
+  it "five" $ run' five `shouldBe` [Atom "5"]
+  it "fives" $ run 10 fives `shouldBe` replicate 10 (Atom "5")
+  it "fivesRev" $ run 10 fivesRev `shouldBe` replicate 10 (Atom "5")
+  it "p56" $ run 10 p56 `shouldBe` concat (replicate 5 [Atom "5",Atom "6"])
+  it "null" $ run' (const $ pure ()) `shouldBe` [Atom "_.0"]
+  it "p010" $ run 2 p010 `shouldBe`
+    [Pair (Atom "_.0") (Pair (Atom "_.1") (Atom "_.0"))
+    ,Pair (Atom "_.0") (Pair (Atom "_.1") (Atom "_.0"))]
